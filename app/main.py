208bd7180ba0269d2a0156e4ee75416e8eaa4154
wonhj12 <wonhj12@naver.com>
2025-09-08 11:41:26 +0900

    feat: 서버 실행시 db 연동하는 로직 구현

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
-from app.routers import root
+from contextlib import asynccontextmanager
+from logging import info
 
-app = FastAPI()
+from app.db.mongodb import MongoDB
+from app.routers import root
+from app.routers import test
+
+
+@asynccontextmanager
+async def db_lifespan(app: FastAPI):
+    db = MongoDB()
+    await db.connect_database()
+    await db.check_status()
+    info("Connected to database")
+
+    yield
+
+    db.get_client().close()
+
+
+app = FastAPI(lifespan=db_lifespan)
 
 app.include_router(root.router)
+app.include_router(test.router)
