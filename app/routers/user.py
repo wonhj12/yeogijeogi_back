49f763c134ef535f028493da4e91263f2c4f5df3
wonhj12 <wonhj12@naver.com>
2025-09-10 17:29:28 +0900

    feat: user 라우터 구현 - service 구현

---
+++ app/routers/user.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+
+from app.core.auth import get_uuid
+from app.db.dao.user_dao import UserDAO
+from app.services.user_service import UserService
+
+router = APIRouter(
+    prefix="/user",
+    tags=["user"],
+)
+
+
+def get_user_service(user_id: str = Depends(get_uuid)):
+    return UserService(UserDAO(user_id))
+
+
+@router.post(
+    "/",
+    responses={
+        201: {"message": "User created successfully"},
+        401: {"message": "Invalid Token"},
+        500: {"message": "Internal Server Error"},
+    },
+)
+# 유저 등록
+async def create_user(user_service: UserService = Depends(get_user_service)):
+    await user_service.create_user()
+    return True
---
+++ app/services/user_service.py
@@ -0,0 +1,11 @@
+from app.core.firebase import get_auth
+from app.db.interface.user_interface import UserInterface
+
+
+class UserService:
+    def __init__(self, user_database: UserInterface):
+        self.user_database = user_database
+
+    async def create_user(self) -> bool:
+        await self.user_database.create_user()
+        return True
