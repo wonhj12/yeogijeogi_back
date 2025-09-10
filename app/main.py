eb305a90cc097d4ed44f1993fdeefb2e84d4c20a
wonhj12 <wonhj12@naver.com>
2025-09-10 17:29:50 +0900

    feat: firebase 연결 - user router 연결

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
+import os
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from logging import info
+from firebase_admin import initialize_app, credentials
 
+from app.core.config import get_settings
 from app.db.mongodb import MongoDB
-from app.routers import root
+from app.routers import root, user
 from app.routers import test
 
 
 @asynccontextmanager
-async def db_lifespan(app: FastAPI):
+async def db_lifespan(_: FastAPI):
+    settings = get_settings()
+    if not settings.firebase_auth:
+        raise Exception("Firebase Credential Location not found in .env")
+    if not os.path.isfile(f"./{settings.firebase_auth}"):
+        raise Exception(
+            f"Firebase Credential File not found in {settings.firebase_auth}"
+        )
+    cred = credentials.Certificate(settings.firebase_auth)
+    initialize_app(cred)
+    info("Connected to Firebase")
+
     db = MongoDB()
     await db.connect_database()
     await db.check_status()
@@ -22,4 +36,5 @@ async def db_lifespan(app: FastAPI):
 app = FastAPI(lifespan=db_lifespan)
 
 app.include_router(root.router)
+app.include_router(user.router)
 app.include_router(test.router)
