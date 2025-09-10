9f4fd66bc0a66403eb4b4419ec42fac5a8d805f8
wonhj12 <wonhj12@naver.com>
2025-09-10 17:45:53 +0900

    mod: create_user의 return type을 None으로 설정

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -6,6 +6,5 @@ class UserService:
     def __init__(self, user_database: UserInterface):
         self.user_database = user_database
 
-    async def create_user(self) -> bool:
+    async def create_user(self) -> None:
         await self.user_database.create_user()
-        return True
