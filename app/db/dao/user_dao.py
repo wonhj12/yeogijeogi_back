2d3c5ab48e38b0095f711a5c4fc13532f36eb155
wonhj12 <wonhj12@naver.com>
2025-09-13 17:15:53 +0900

    feat: db 사용자 삭제 로직 구현 - 연결된 데이터 모두 삭제하도록 로직 구현

--- app/db/dao/user_dao.py
+++ app/db/dao/user_dao.py
@@ -1,7 +1,12 @@
+from beanie import BulkWriter
 from fastapi import HTTPException
 
 from app.db.interface.user_interface import UserInterface
+from app.db.models.coordinate import Coordinate
 from app.db.models.users import Users
+from app.db.models.walk_points import WalkPoints
+from app.db.models.walk_summary import WalkSummary
+from app.db.models.walks import Walks
 
 
 class UserDAO(UserInterface):
@@ -17,3 +22,24 @@ class UserDAO(UserInterface):
         if await self.check_user_exists():
             raise HTTPException(status_code=409, detail="user-already-exists")
         await Users(id=self.user_id).insert()
+
+    async def delete_user(self) -> None:
+        if not await self.check_user_exists():
+            raise HTTPException(status_code=404, detail="user-not-found")
+
+        user = await Users.find_one(Users.id == self.user_id)
+        w_list = await Walks.find(Walks.user_id == user.id).to_list()
+
+        async with BulkWriter() as bulk_writer:
+            for w in w_list:
+                wp_list = await WalkPoints.find(WalkPoints.walk_id == w).to_list()
+                for wp in wp_list:
+                    await wp.delete(bulk_writer=bulk_writer)
+
+                ws_list = await WalkSummary.find(WalkSummary.walk_id == w).to_list()
+                for ws in ws_list:
+                    await ws.delete(bulk_writer=bulk_writer)
+
+                await w.delete(bulk_writer=bulk_writer)
+
+            await user.delete(bulk_writer=bulk_writer)
