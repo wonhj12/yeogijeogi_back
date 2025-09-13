435d1d7f6585082a7bb181ea5adf78e722e7b321
wonhj12 <wonhj12@naver.com>
2025-09-14 08:59:38 +0900

    remove: 사용하지 않는 mongodb 관련 파일 제거

--- app/db/interface/walk_summary_interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from app.schemas.walk_schema.base_schema import UserWalkSummary
-
-
-class WalkSummaryInterface(ABC):
-    @abstractmethod
-    async def get_user_walk_summary(self, user_id: str) -> UserWalkSummary:
-        pass
--- app/schemas/walk_schema/base_schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class UserWalkSummary(BaseModel):
-    walk_distance: float  # 총 산책 거리 (km 단위)
-    walk_time: int  # 총 산책 시간 (분 단위)
