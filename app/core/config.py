dbaa934027e227b622863549832f442dd5ea883a
wonhj12 <wonhj12@naver.com>
2025-09-13 19:33:20 +0900

    mod: mongodb 대신 postgresql 사용하도록 변경 - mondgodb 제거 - database (postgresql) 추가

--- app/core/config.py
+++ app/core/config.py
@@ -6,9 +6,8 @@ env_path = Path(__file__).resolve().parent.parent / ".env"
 
 
 class Settings(BaseSettings):
-    mongo_uri: str
-    mongo_database_name: str
     model_config = SettingsConfigDict(env_file=env_path, env_file_encoding="utf-8")
+    db_uri: str
     firebase_auth: str
 
 
---
+++ app/db/database.py
@@ -0,0 +1,19 @@
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import get_settings
+
+settings = get_settings()
+if not settings.db_uri:
+    raise Exception("Database URI not found on .env")
+
+engine = create_async_engine(settings.db_uri, future=True)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
+
+
+@asynccontextmanager
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
